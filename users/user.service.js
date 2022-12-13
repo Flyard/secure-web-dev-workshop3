883bc57baa8c73304d50e548cb3cb3468b56e8fa
Florian RANAIVOSON <ranaivosonflorian@gmail.com>
2022-12-13 08:58:22 +0100

    add : create, find, check, update, delete, token

--- users/user.service.js
+++ users/user.service.js
@@ -1,22 +1,97 @@
 const User = require('./user.model');
 const bcrypt = require('bcrypt');
+const passport = require('passport');
 
 
-async function register(username, password) {
-    const hash = await bcrypt.hash(password, 10);
-    const user = new User({username, password: hash});
-    return await user.save();
+async function createUser(username, password) {
+    try {
+        const hash = await bcrypt.hash(password, 10);
+        const user = await new User({username, password: hash});
+        console.log(`[+] Added user : ${username}:${password}`);
+        return user;
+
+    } catch (err) {
+        console.log("Not created");
+        return null;
+    }
+
 }
 
-async function findByUsername() {
+
+async function findAll() {
     try {
-        return User.findOne({username: username});
-    } catch(err) {
+        return User.find({}).select('username');
+    } catch (err) {
+        console.log("Not found");
         console.error(err);
+        return false;
     }
 }
+
+async function findUser(id) {
+    try {
+        return await User.findOne({id});
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
+}
+
+async function checkUser(username) {
+    try {
+        return await User.findOne({username});
+    } catch (err) {
+        console.log("[!] Error");
+        console.error(err);
+        return null;
+    }
+}
+
+async function updateUser(id, property) {
+    try {
+        if (property.role) delete property.role;
+        await User.findOneAndUpdate({id}, property);
+        return await getUser(id);
+    } catch (err) {
+        console.log("[!] Error");
+        console.error(err);
+        return null;
+    }
+}
+
+async function deleteUser(id) {
+    try {
+        return await User.findOneAndDelete({_id:id});
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
+}
+
+async function verify(username, password) {
+    try {
+        const user = await User.findOne({username});
+        const match = await bcrypt.compare(password, user.password);
+        return match;
+    } catch (err) {
+        console.log("Wrong credentials");
+        console.error(err);
+        return null;
+    }
+}
+
+async function tokenGen(username) {
+    return jwt.sign({sub:username}, process.env.JWT_SECRET);
+}
+
 module.exports = {
-	register,
-    findByUsername
+    createUser,
+    findAll,
+    findUser,
+    checkUser,
+    updateUser,
+    deleteUser,
+    verify, 
+    tokenGen
 };
 
