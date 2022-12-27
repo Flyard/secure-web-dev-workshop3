0562340fb4736c5199207c9975d9cdc083a4becd
Florian RANAIVOSON <ranaivosonflorian@gmail.com>
2022-12-27 19:35:46 +0100

    feat: local strategy

--- index.js
+++ index.js
@@ -2,18 +2,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const passport = require('passport');
+require()
 //FOLDERS
-const locationController = require('./locations/locations.controller')
+const locationController = require('./locations/locations.controller');
+const userController = require('./user/users.controller');
 
 //PARAMETERS
 require('dotenv').config();
 const app = express();
 const port = 3000;
 
-
+app.use(express.json());
 app.use(bodyParser.json());
+app.use(passport.initialize());
 app.use(locationController);
+app.use(userController);
 //app.use(locationController);
 app.listen(port, () => {
 	console.log(`API listening on port ${port}, visit http://localhost:${port}/`)
